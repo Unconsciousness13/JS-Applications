65f26320517f50860417df602c1019984141c7fa
Plamen Iliev <pakounicredit@gmail.com>
2021-10-29 11:30:02 +0200

    Asynchronous Programming

--- Asynchronous Programming/01.XHR/scripts/app.js
+++ Asynchronous Programming/01.XHR/scripts/app.js
@@ -1,3 +1,34 @@
+// we have to add id load in to the button in index.html file
+
+
 function loadRepos() {
-   console.log("TODO...");
+    let button = document.querySelector('#load');
+    button.addEventListener('click', function loadRepos() {
+        let url = 'https://api.github.com/users/testnakov/repos';
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.addEventListener('readystatechange', function() {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                document.getElementById('res').textContent = httpRequest.responseText;
+            }
+        });
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    });
+
+}
+
+// way without adding the id load
+
+function loadRepos() {
+    const url = 'https://api.github.com/users/testnakov/repos';
+
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.addEventListener('readystatechange', () => {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            document.getElementById('res').textContent = httpRequest.responseText;
+        }
+    });
+
+    httpRequest.open('GET', url);
+    httpRequest.send();
 }
