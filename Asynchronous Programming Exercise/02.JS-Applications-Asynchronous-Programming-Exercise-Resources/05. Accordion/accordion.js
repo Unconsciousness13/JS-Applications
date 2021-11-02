7a3b75ae8ec2239339f2b7ca17a2186542b14f0c
Plamen Iliev <pakounicredit@gmail.com>
2021-11-02 18:24:02 +0100

    Exercise

--- Asynchronous Programming Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
+++ Asynchronous Programming Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
@@ -1,3 +1,77 @@
 function solution() {
-    //TODO .....
+    const mainDiv = document.getElementById('main');
+
+    // async function takeParagraphContent(id) {
+    //     console.log(id)
+    //     const url = `http://localhost:3030/jsonstore/advanced/articles/details/${id}`;
+    //     const res = await fetch(url);
+    //     const data = await res.json();
+
+    //     return data;
+    // }
+
+    async function getArticles() {
+        const url = 'http://localhost:3030/jsonstore/advanced/articles/list'
+        const res = await fetch(url);
+        const data = await res.json();
+
+
+        data.forEach(obj => {
+            const searchId = obj._id;
+
+            const divAccordion = document.createElement('div');
+            divAccordion.className = 'accordion';
+            mainDiv.appendChild(divAccordion);
+
+            const divHead = document.createElement('div');
+            divHead.className = 'head';
+            divAccordion.appendChild(divHead);
+
+            const headSpan = document.createElement('span');
+            headSpan.textContent = `${obj.title}`
+            divHead.appendChild(headSpan);
+
+            const moreButton = document.createElement('button');
+            moreButton.className = 'button';
+            moreButton.id = searchId
+            moreButton.textContent = 'More';
+            divHead.appendChild(moreButton);
+
+            moreButton.addEventListener('click', showHidden)
+
+            // extra content
+            const divExtra = document.createElement('div');
+            divExtra.className = 'extra';
+            divExtra.style.display = 'none'
+
+            const moreContent = document.createElement('p');
+
+            // moreContent.textContent = takeParagraphContent(searchId);
+            divExtra.appendChild(moreContent);
+
+            divAccordion.appendChild(divExtra);
+
+
+            function showHidden() {
+                if (moreButton.textContent === 'More') {
+                    fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${searchId}`)
+                        .then(res => res.json())
+                        .then(extraContent => {
+                            moreContent.textContent = extraContent.content;
+                            moreButton.textContent = 'Less';
+                            divExtra.style.display = 'block';
+                        });
+                } else {
+                    moreContent.textContent = '';
+                    moreButton.textContent = 'More';
+                    divExtra.style.display = 'none';
+                }
+            }
+
+
+
+        });
+
+    }
+    getArticles();
 }
