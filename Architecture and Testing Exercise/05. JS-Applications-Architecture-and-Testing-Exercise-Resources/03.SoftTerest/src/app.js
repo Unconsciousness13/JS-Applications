04afffa31c2aee4fc90b2b8b7fec95dc08a73f8c
Plamen Iliev <pakounicredit@gmail.com>
2021-11-14 18:04:08 +0100

    Exercise

--- Architecture and Testing Exercise/05. JS-Applications-Architecture-and-Testing-Exercise-Resources/03.SoftTerest/src/app.js
+++ Architecture and Testing Exercise/05. JS-Applications-Architecture-and-Testing-Exercise-Resources/03.SoftTerest/src/app.js
@@ -1,3 +1,50 @@
-import * as api from './api/data.js';
+import { showSection } from './dom.js';
+import { showHomePage } from './views/home.js';
+import { showCreatePage } from './views/create.js';
+import { showCatalogPage } from './views/catalog.js';
+import { showLoginPage } from './views/login.js';
+import { showRegisterPage } from './views/register.js';
+import { showDetailsPage } from './views/details.js';
 
-window.api = api;
+
+const links = {
+    'homeLink': 'home',
+    'getStartedLink': 'home',
+    'catalogLink': 'catalog',
+    'loginLink': 'login',
+    'registerLink': 'register',
+    'createLink': 'create'
+};
+
+const views = {
+    'home': showHomePage,
+    'catalog': showCatalogPage,
+    'login': showLoginPage,
+    'register': showRegisterPage,
+    'create': showCreatePage,
+    'details': showDetailsPage
+
+};
+
+const nav = document.querySelector('nav');
+nav.addEventListener('click', onNavigate);
+
+const ctx = {
+    goTo,
+    showSection
+};
+
+function onNavigate(event) {
+    const name = links[event.target.id];
+    if (name) {
+        event.preventDefault();
+        goTo(name);
+    }
+}
+
+function goTo(name, ...params) {
+    const view = views[name];
+    if (typeof view == 'function') {
+        view(ctx, ...params);
+    }
+}
