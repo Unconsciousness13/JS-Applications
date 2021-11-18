f3ebd6d5a4c8f0bf8013d2267e802b4a3319fb4e
Plamen Iliev <pakounicredit@gmail.com>
2021-11-18 12:45:15 +0100

    Exercise

--- Client Side Rendering Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/05.Table-Search/solution.js
+++ Client Side Rendering Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/05.Table-Search/solution.js
@@ -1,8 +1,32 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        //   TODO:
 
-   }
+    }
+}
+
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const studentRow = (student) => html `<tr>
+<td>${student.firstName} ${student.lastName}</td>
+<td>${student.email}</td>
+<td>${student.course}</td>
+</tr>`;
+
+let data;
+
+start();
+
+async function start() {
+    const res = await fetch('http://localhost:3030/jsonstore/advanced/table');
+    data = Object.values(await res.json());
+
+    update();
+}
+
+
+function update() {
+    render(data.map(studentRow), document.querySelector('tbody'));
 }
