39e6952cad7005abde7fd6f8f2c6c41e4cc6b996
Plamen Iliev <pakounicredit@gmail.com>
2021-11-18 15:24:19 +0100

    Exercise

--- Client Side Rendering Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/06.Book-Library/src/app.js
+++ Client Side Rendering Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/06.Book-Library/src/app.js
@@ -1,1 +1,19 @@
-console.log('My requests...')
+import { showCatalog } from './catalog.js';
+import { render } from './utility.js';
+import { showCreate } from './create.js';
+import { showUpdate } from './update.js';
+
+const root = document.body;
+
+const ctx = {
+    update
+};
+update();
+
+function update() {
+    render([
+        showCatalog(ctx),
+        showCreate(ctx),
+        showUpdate(ctx)
+    ], root)
+}
---
+++ Client Side Rendering Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/06.Book-Library/src/create.js
@@ -0,0 +1,32 @@
+import { html, createBook } from './utility.js';
+
+const createTemplate = (onSuccess) => html `<form  @submit=${ev => onSubmit(ev,onSuccess)} id="add-form">
+<h3>Add book</h3>
+<label>TITLE</label>
+<input type="text" name="title" placeholder="Title...">
+<label>AUTHOR</label>
+<input type="text" name="author" placeholder="Author...">
+<input type="submit" value="Submit">
+</form>`;
+
+export function showCreate(ctx) {
+    if (ctx.books == undefined) {
+        return createTemplate(ctx.update);
+    } else {
+        return null;
+    }
+
+}
+
+async function onSubmit(event, onSuccess) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const title = formData.get('title').trim();
+    const author = formData.get('author').trim();
+
+    await createBook({ title, author });
+
+    event.target.reset();
+    onSuccess();
+}
