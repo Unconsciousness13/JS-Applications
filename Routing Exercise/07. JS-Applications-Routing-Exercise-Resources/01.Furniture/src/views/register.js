3896113df8ba943d88bb5e2ad12d811b50c7d898
Plamen Iliev <pakounicredit@gmail.com>
2021-11-22 10:37:39 +0100

    Exercise

--- Routing Exercise/07. JS-Applications-Routing-Exercise-Resources/01.Furniture/src/views/register.js
+++ Routing Exercise/07. JS-Applications-Routing-Exercise-Resources/01.Furniture/src/views/register.js
@@ -2,26 +2,28 @@ import { register } from '../api/api.js';
 import { html } from '../lib.js';
 
 
-const registerTemplate = (onSubmit) => html `<div class="row space-top">
+const registerTemplate = (onSubmit, errorMsg, errors) => html `
+<div class="row space-top">
 <div class="col-md-12">
     <h1>Register New User</h1>
     <p>Please fill all fields.</p>
 </div>
 </div>
-<form>
-<div @submit=${onSubmit}class="row space-top">
+<form @submit=${onSubmit}>
+<div class="row space-top">
     <div class="col-md-4">
+        ${errorMsg ? html`<div class="form-group error">${errorMsg}</div>` :null}
         <div class="form-group">
             <label class="form-control-label" for="email">Email</label>
-            <input class="form-control" id="email" type="text" name="email">
+            <input class=${'form-control' + (errors.email ? ' is-invalid' : '')} id="email" type="text" name="email">
         </div>
         <div class="form-group">
             <label class="form-control-label" for="password">Password</label>
-            <input class="form-control" id="password" type="password" name="password">
+            <input class=${'form-control' + (errors.password ? ' is-invalid' : '')} id="password" type="password" name="password">
         </div>
         <div class="form-group">
             <label class="form-control-label" for="rePass">Repeat</label>
-            <input class="form-control" id="rePass" type="password" name="rePass">
+            <input class=${'form-control' + (errors.rePass ? ' is-invalid' : '')} id="rePass" type="password" name="rePass">
         </div>
         <input type="submit" class="btn btn-primary" value="Register" />
     </div>
@@ -29,10 +31,10 @@ const registerTemplate = (onSubmit) => html `<div class="row space-top">
 </form>
 `;
 export function registerPage(ctx) {
-    update();
+    update(null, {});
 
-    function update(errorMsg) {
-        ctx.render(registerTemplate(onSubmit, errorMsg));
+    function update(errorMsg, errors) {
+        ctx.render(registerTemplate(onSubmit, errorMsg, errors));
     }
 
     async function onSubmit(event) {
@@ -43,21 +45,32 @@ export function registerPage(ctx) {
         const password = formData.get('password').trim();
         const rePass = formData.get('rePass').trim();
 
-
-
         try {
-
             if (email == '' || password == '') {
-                throw new Error('All fields are required');
+                throw {
+                    error: new Error('All fields are required'),
+                    errors: {
+                        email: email == '',
+                        password: password == ''
+                    }
+                };
 
             }
             if (password != rePass) {
-                throw new Error('Passwords do not match');
+                throw {
+                    error: new Error('Passwords do not match'),
+                    errors: {
+                        password: true,
+                        rePass: true
+                    }
+                };
+                
             }
             await register(email, password);
             ctx.page.redirect('/');
         } catch (err) {
-            update(err.message);
+            const message = err.message || err.error.message;
+            update(message, err.errors || {});
         }
     }
 }
