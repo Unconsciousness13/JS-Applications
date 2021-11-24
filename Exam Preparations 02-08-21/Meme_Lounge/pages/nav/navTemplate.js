2da52274bb42d0f30f589acac30698802a023ac8
Plamen Iliev <pakounicredit@gmail.com>
2021-11-24 12:09:03 +0100

    Exam prep

---
+++ Exam Preparations 02-08-21/Meme_Lounge/pages/nav/navTemplate.js
@@ -0,0 +1,22 @@
+import { html } from './../../node_modules/lit-html/lit-html.js';
+
+
+export let navTemplate = () => html `
+<a href="#">All Memes</a>
+             <!-- Logged users -->
+            <div class="user">
+                <a href="#">Create Meme</a>
+                <div class="profile">
+                    <span>Welcome, {email}</span>
+                    <a href="#">My Profile</a>
+                    <a href="#">Logout</a>
+                </div>
+            </div>
+            <!-- Guest users -->
+            <div class="guest">
+                <div class="profile">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+                <a class="active" href="#">Home Page</a>
+            </div>`;
