e47ed87b6f98be9de148130680913b8a8b05047d
Plamen Iliev <pakounicredit@gmail.com>
2021-11-24 12:16:49 +0100

    Exam prep

--- Exam Preparations 02-08-21/Meme_Lounge/pages/home/homeTemplate.js
+++ Exam Preparations 02-08-21/Meme_Lounge/pages/home/homeTemplate.js
@@ -1,1 +1,15 @@
 import { html } from './../../node_modules/lit-html/lit-html.js';
+
+
+export let homeTemplate = () => html `
+<section id="welcome">
+    <div id="welcome-container">
+        <h1>Welcome To Meme Lounge</h1>
+        <img src="/images/welcome-meme.jpg" alt="meme">
+        <h2>Login to see our memes right away!</h2>
+        <div id="button-div">
+            <a href="#" class="button">Login</a>
+            <a href="#" class="button">Register</a>
+        </div>
+    </div>
+</section>`;
--- Exam Preparations 02-08-21/Meme_Lounge/pages/login/loginTemplate.js
+++ Exam Preparations 02-08-21/Meme_Lounge/pages/login/loginTemplate.js
@@ -1,1 +1,19 @@
 import { html } from './../../node_modules/lit-html/lit-html.js';
+
+
+export let loginTemplate = () => html `
+<section id="login">
+    <form id="login-form">
+        <div class="container">
+            <h1>Login</h1>
+            <label for="email">Email</label>
+            <input id="email" placeholder="Enter Email" name="email" type="text">
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder="Enter Password" name="password">
+            <input type="submit" class="registerbtn button" value="Login">
+            <div class="container signin">
+                <p>Dont have an account?<a href="#">Sign up</a>.</p>
+            </div>
+        </div>
+    </form>
+</section>`;
