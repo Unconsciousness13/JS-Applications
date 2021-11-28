4544f82efeecba9ba032034f65cd842e2c0a0237
Plamen Iliev <pakounicredit@gmail.com>
2021-11-28 14:26:05 +0100

    Exam prep

--- Exam prep 26-11-21/Online-Books-Library-Resources/src/app.js
+++ Exam prep 26-11-21/Online-Books-Library-Resources/src/app.js
@@ -1,5 +1,15 @@
 import { page, render } from './lib.js';
+import { homePage } from './views/home.js'
+
+const root = document.getElementById('site-content');
 
 
+page(decorateContext);
+page('/', homePage);
 
-console.log('works')
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    next();
+}
