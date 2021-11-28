e54e04e71406d97878272b54537692aae5d85e2c
Plamen Iliev <pakounicredit@gmail.com>
2021-11-28 15:54:38 +0100

    Exam prep

--- Exam prep 26-11-21/Online-Books-Library-Resources/src/app.js
+++ Exam prep 26-11-21/Online-Books-Library-Resources/src/app.js
@@ -1,15 +1,41 @@
 import { page, render } from './lib.js';
 import { homePage } from './views/home.js'
+import { loginPage } from './views/login.js';
+import { getUserData } from '../src/util.js'
+import { logout } from './api/api.js';
 
 const root = document.getElementById('site-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
 
 
 page(decorateContext);
 page('/', homePage);
+page('/login', loginPage);
 
+updateUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
     next();
+}
+
+export function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+        document.querySelector('#user span').textContent = `Welcome, ${userData.email}`
+
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
 }
