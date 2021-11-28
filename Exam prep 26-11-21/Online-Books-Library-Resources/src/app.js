fd44b06073500d79c1f964531e01bcbe91de7acb
Plamen Iliev <pakounicredit@gmail.com>
2021-11-28 16:09:40 +0100

    Exam prep

--- Exam prep 26-11-21/Online-Books-Library-Resources/src/app.js
+++ Exam prep 26-11-21/Online-Books-Library-Resources/src/app.js
@@ -3,6 +3,7 @@ import { homePage } from './views/home.js'
 import { loginPage } from './views/login.js';
 import { getUserData } from '../src/util.js'
 import { logout } from './api/api.js';
+import { registerPage } from './views/register.js';
 
 const root = document.getElementById('site-content');
 document.getElementById('logoutBtn').addEventListener('click', onLogout);
@@ -11,6 +12,7 @@ document.getElementById('logoutBtn').addEventListener('click', onLogout);
 page(decorateContext);
 page('/', homePage);
 page('/login', loginPage);
+page('/register', registerPage);
 
 updateUserNav();
 page.start();
