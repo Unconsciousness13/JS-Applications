6628eaccae0f71441b1d1978fd16554b8728ff93
Plamen Iliev <pakounicredit@gmail.com>
2021-12-01 11:26:53 +0100

    Exam prep

--- Exam prep Cars Tube/Cars/src/api/data.js
+++ Exam prep Cars Tube/Cars/src/api/data.js
@@ -28,15 +28,3 @@ export async function editCar(id, car) {
 export async function deleteCar(id) {
     return api.del('/data/cars/' + id);
 }
-
-// export async function likeBook(bookId) {
-//     return api.post('/data/likes', { bookId });
-// }
-
-// export async function getLikesByBookId(bookId) {
-//     return api.get(`/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`);
-// }
-
-// export async function getMyLikeByBookId(bookId, userId) {
-//     return api.get(`/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
-// }
